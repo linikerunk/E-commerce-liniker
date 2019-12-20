2932b957a7351b4a3c6684fb8d85eaa587a74167
linikerunk <linikerenem@gmail.com>
2019-12-20 01:57:59 -0300

    'correções-de-bugs'

--- perfil/views.py
+++ perfil/views.py
@@ -65,6 +65,11 @@ class BasePerfil(View):
 class Criar(BasePerfil):
     def post(self, *args, **kwargs):
         if not self.userform.is_valid() or not self.perfilform.is_valid():
+            messages.error(
+                self.request,
+                'Existem erros no formulário de cadastro. Verifique se todos '
+                'os campos foram preenchidos corretamentes'
+            )
             return self.renderizar
         
         username = self.userform.cleaned_data.get('username')
@@ -127,7 +132,7 @@ class Criar(BasePerfil):
             self.request,
             'Você fez login e pode concluir sua compra.')
 
-        return redirect('perfil:criar')
+        return redirect('produto:carrinho')
         return self.renderizar
     
 
