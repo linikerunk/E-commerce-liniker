8318a531e53decee63fc8303fa8a0459a97715f9
Oliveira <uic05644@contiwan.com>
2019-12-20 12:56:14 -0300

    '17:52'

--- pedido/views.py
+++ pedido/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
+from produto.models import Variacao
+
 
 # Create your views here.
 
 class Pagar(View):
+    template_name = 'pedido/pagar.html'
+
     def get(self, *args, **kwargs):
-        return HttpResponse('Pagar')
+        if not self.request.user.is_authenticated:
+            messages.error(
+            self.request,
+            'Você precisa realizar login'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+            self.request,
+            'Carrinho vazio.'
+            )
+            return redirect('produto:lista')
+
+        carrinho = self.request.session.get('carrinho')
+        carrinho_variacao_ids = [v for v in carrinho]
+        bd_variacoes = list(Variacao.objects.select_related('produto').filter(id__in=carrinho_variacao_ids))
+
+
+        for variacao in bd_variacoes:
+            vid = variacao.id
+            estoque = variacao.estoque
+            qtd_carrinho = carrinho[vid]['quantidade']
+            preco_unt = carrinho[vid]['preco_unitario']
+            preco_unt_promo = carrinho[vid]['preco_unitario_promocional']
+            
+            
+        contexto = {
+
+        }
+        return render(self.request, self.template_name, contexto)
+
 
 
 class SalvarPedido(View):
